05327be612aa9e37a7728d34d53bec705e44afec
Rafi Zaman Saukhin <rafizamans10@gmail.com>
2024-11-18 12:39:40 +0600

    http basics

--- 2-ExpressJS/app.js
+++ 2-ExpressJS/app.js
@@ -2,6 +2,27 @@
 const http = require('http');
 
 const server = http.createServer((req, res)=>{
-    
+    // console.log(req.method)
+    const url = req.url;
+    if( url === '/'){
+        // home page setup 
+        res.writeHead(200, {'content-type': 'text/html'});
+        res.write('<h1>Home Page</h1>');
+        res.end();
+    }
+    else if( url === '/about'){
+        // about page 
+        res.writeHead(200, {'content-type': 'text/html'});
+        res.write('<h1>About Page</h1>');
+        res.end();
+        
+    }else{ //404
+        res.writeHead(404, {'content-type':'text/html'});
+        res.write('<h1>404 not found</h1>');
+        res.end();
+    } 
 })
+
+server.listen(5000);
+
 
